aa8abb49db0fef01f413588e5e6dade5b0efe537
zurnachyan media <gevzu87@gmail.com>
2025-04-21 10:01:33 +0400

    Upload

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama2",
+      "name": "panorama2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4087,
+      "initialViewParameters": {
+        "yaw": 0.10977843252904584,
+        "pitch": -0.03140306368227286,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7208282360560894,
+          "pitch": 0.07759308726206271,
+          "rotation": 0,
+          "target": "0-panorama2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama1",
+      "name": "panorama1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.4587311943577035,
+        "pitch": -0.017860785289652625,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.166638777169778,
+          "pitch": 0.2102350453209585,
+          "rotation": 0,
+          "target": "1-panorama1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Comuna Sevan",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
